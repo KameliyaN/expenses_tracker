75882cd3dd8bc378ac4ef7550bf61036297b0985
KameliyaN <72250856+KameliyaN@users.noreply.github.com>
2020-10-21 19:24:39 +0300

    dtl and html changes

--- e_tracker/views.py
+++ e_tracker/views.py
@@ -2,25 +2,17 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-from e_tracker.forms import ProfileForm
+
 from e_tracker.models import Profile
 
 
 def home_page(request):
     if request.method == 'POST':
-        form = ProfileForm(request.POST)
-        if form.is_valid():
-            person = form.save()
-            person.save()
-            context={
-                'form':form
-            }
-            return render(request, 'e_tracker/profile.html',context)
-    else:
-        smtng = request.GET.get(profile_page(request))
-        if smtng:
-            return render(request, 'e_tracker/home-with-profile.html')
+        return render(request, 'e_tracker/home-with-profile.html')
+    elif request.method=='GET':
         return render(request, 'e_tracker/home-no-profile.html')
+    else:
+        return render(request, 'e_tracker/home-with-profile.html')
 
 
 def create_expense(request):
