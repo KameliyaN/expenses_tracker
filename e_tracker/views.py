3066160360e75ca980cac931df0d61f7420c472d
KameliyaN <72250856+KameliyaN@users.noreply.github.com>
2020-10-23 09:03:01 +0300

    bac to func based views

--- e_tracker/views.py
+++ e_tracker/views.py
@@ -9,61 +9,43 @@ from e_tracker.forms import ProfileForm
 from e_tracker.models import Profile, Expense
 
 
-# @login_required
-# def home_page(request):
-#     if request.method == 'POST':
-#         form = ProfileForm(request.POST)
-#         last_name = request.POST.get('last_name')
-#         if form.is_valid():
-#
-#             # if Profile.objects.get(last_name=last_name):
-#             try:
-#                 profile = Profile.objects.get(last_name)
-#                 if profile.expense.all():
-#                     context = {
-#                         'profile': profile,
-#                         'expenses': profile.expense.all()
-#                     }
-#                     return render(request, 'e_tracker/home-with-profile.html', context)
-#                 else:
-#                     context = {
-#
-#                         'expenses': profile.expense.all()
-#                     }
-#                     return render(request, 'e_tracker/home-with-profile.html', context)
-#
-#
-#             except:
-#                 profile = form.save()
-#                 profile.save()
-#                 context = {'expenses': profile.expense.all()
-#                            }
-#                 return render(request, 'e_tracker/home-with-profile.html', context)
-#     elif request.method == 'GET' and request.GET:
-#         profile = Profile(request.GET)
-#         context = {'profile': profile}
-#
-#         return render(request, 'e_tracker/home-with-profile.html', context)
-#
-#     return render(request, 'e_tracker/home-no-profile.html')
+@login_required
 def home_page(request):
     if request.method == 'POST':
-        pass
+        form = ProfileForm(request.POST)
+        last_name = request.POST.get('last_name')
+        if form.is_valid():
 
-    if request.user.is_authenticated:
-       pass
+            # if Profile.objects.get(last_name=last_name):
+            try:
+                profile = Profile.objects.get(last_name)
+                if profile.expense.all():
+                    context = {
+                        'profile': profile,
+                        'expenses': profile.expense.all()
+                    }
+                    return render(request, 'e_tracker/home-with-profile.html', context)
+                else:
+                    context = {
+
+                        'expenses': profile.expense.all()
+                    }
+                    return render(request, 'e_tracker/home-with-profile.html', context)
+
+
+            except:
+                profile = form.save()
+                profile.save()
+                context = {'expenses': profile.expense.all()
+                           }
+                return render(request, 'e_tracker/home-with-profile.html', context)
+    elif request.method == 'GET' and request.GET:
+        profile = Profile(request.GET)
+        context = {'profile': profile}
+
+        return render(request, 'e_tracker/home-with-profile.html', context)
+
     return render(request, 'e_tracker/home-no-profile.html')
-
-
-class UserDetail(generic.DetailView):
-    model = Profile
-    template_name = 'e_tracker/profile.html'
-    context_object_name = 'user'
-
-
-# def redirect_user(request):
-#     url = f'e_tracker/profile.html'
-#     return HttpResponseRedirect(url)
 
 
 def create_expense(request):
@@ -83,4 +65,8 @@ def profile_edit(request):
 
 
 def profile_delete(request):
+    return None
+
+
+def profile(request):
     return None
